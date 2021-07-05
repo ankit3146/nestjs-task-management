b206ea939091914de61678c1ec1f8bf6e8cd3ef1
Ankit Varshney <an225884@dal.ca>
2021-07-05 17:05:32 -0400

    Added the feature for search by filter

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task-dto';
+import { GetTaskFilterDto } from './dto/get-task-filter-dto';
 
 @Injectable()
 export class TasksService {
@@ -37,5 +38,24 @@ export class TasksService {
     this.tasks[index].status = status;
 
     return this.tasks[index];
+  }
+
+  getTasksWithFilters(filterDto: GetTaskFilterDto): Task[] {
+    const { status, search } = filterDto;
+
+    let tasks = this.getAllTasks();
+
+    if (status) {
+      tasks = tasks.filter((task) => task.status === status);
+    }
+
+    if (search) {
+      tasks = tasks.filter(
+        (task) =>
+          task.title.includes(search) || task.description.includes(search),
+      );
+    }
+
+    return tasks;
   }
 }
